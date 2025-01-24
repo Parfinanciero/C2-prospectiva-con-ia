ee6f6f06692590af830cea4d0a3f4f2417af34f1
BUKODONOZOR <>
2025-01-24 15:21:48 -0500

    MVP commit

--- ProspectivaApi/api/service.py
+++ ProspectivaApi/api/service.py
@@ -1,10 +1,28 @@
-import json 
+from fastapi.responses import PlainTextResponse
+import google.generativeai as genai
+import json
 
-with open('api/ejemplo.json', 'r') as archivo:
-    contenido_json = json.load(archivo)  
+def generate_response(data):
+    genai.configure(api_key="AIzaSyC9ZPx7CvNEa_CBo6_IUTaA1kd7JQ6vZjA") 
+
+    generation_config = {
+    "temperature": 1.3,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 20000,
+    "response_mime_type": "text/plain",
+    }
 
 
-json_como_cadena = json.dumps(contenido_json)
+    json_como_cadena = json.dumps(data)
 
+    model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash-exp",
+    generation_config=generation_config,
+    )
 
-print(json_como_cadena)
+    chat_session = model.start_chat(history=[])
+
+    response = chat_session.send_message("aplica todos los analisis adicionales , y reflejalos de manera numerica con el mismo usuario , retorna un archivo json"+ json_como_cadena +"recuerda solo mostrar los siguientes datos : gastos total 1800 categorias que debe tener nombre total porcentaje lo arganizas por fecha y muestras fecha total analisis_adicional  gasto_promedio_diario  variacion_gasto_diario  categoria_mayor_gasto debe tener nombre , total categoria_menor_gasto que debe tener nombre y total , la tendencia de gasto que debe ser decreceinte o creciente segun tu analisis y meta que debe tener , titulo ,monto de meta , monto actual progreso de porcentaje diferencia de monto proyeccion tiempo dias restantes estimados progreso en el tiempo ,la cadena que recibiras con los datos va a cambiar con el tiempo , pero por ahora no puedes cambiar el formato hasta que yo te lo diga explicitamente ")
+    
+    return PlainTextResponse(content=response.text) 
